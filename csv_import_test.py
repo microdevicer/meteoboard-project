6ed0607aded36ed128452268925265dd09f636ba
microdevicer <microdevicer@gmail.com>
2013-02-01 23:18:56 -0800

    Create csv_import_test.py

---
+++ csv_import_test.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+
+from meteoboardlib import *
+import csv
+
+c = csv.writer(open("MYFILE.csv", "wb"))    
+port = getMeteoBoard_port() # узнаем к какому порту подключена плата
+
+val =[]
+    
+#print str(getDHT11_Hum(port)) +' '+ str(getDHT11_temp(port)) + ' '+ str(getBMP085_temp(port)) + ' '+ str(getBMP085_pressure(port)) + ' '+ str(getBMP085_altitude(port))
+
+print u'Влажность, % = ', getDHT11_temp(port)
+print u'Температура, C = ',getDHT11_Hum(port)
+print u'Температура, C = ',getBMP085_temp(port)
+print u'Давление, Па = ',getBMP085_pressure(port)
+print u'Альтитуда, м = ',getBMP085_altitude(port)
+
+#c.writerow(["humidity,% ","Temperature,C ","Temperature,C ","Pressure,Pa ","altitude,m"])
+
+#for i in range(500):
+#    print i
+#    c.writerow([str(getDHT11_temp(port)),str(getDHT11_Hum(port)),str(getBMP085_temp(port)),str(getBMP085_pressure(port)), str(getBMP085_altitude(port))])
+    
