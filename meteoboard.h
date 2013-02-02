ea95514a22040db1bd88fea73df136cf84dec3fd
microdevicer <microdevicer@gmail.com>
2013-02-01 22:55:21 -0800

    Create meteoboard.h

---
+++ meteoboard.h
@@ -0,0 +1,7 @@
+
+int getMeteoBoard_port(void);
+int getDHT11_temp(unsigned char cport_num);
+int getDHT11_Hum(unsigned char cport_num);
+int getBMP085_temp(unsigned char cport_num);
+int getBMP085_pressure(unsigned char cport_num);
+int getBMP085_altitude(unsigned char cport_num);
